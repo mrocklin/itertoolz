c752fbffe98f420649981d679038e1bfb27a30a1
Matthew Rocklin <mrocklin@gmail.com>
2013-08-23 13:53:20 -0500

    add countby

--- itertoolz/recipes.py
+++ itertoolz/recipes.py
@@ -1,9 +1,29 @@
 from itertoolz.core import groupby, identity
 
+def countby(func, seq):
+    """ Group elements of a collection by a key function
+
+    >>> countby(len, ['cat', 'mouse', 'dog'])
+    {3: 2, 5: 1}
+
+    >>> def even(x): return x % 2 == 0
+    >>> countby(even, [1, 2, 3])  # doctest:+SKIP
+    {True: 1, False: 2}
+
+    See Also:
+        groupby
+    """
+    return dict([(k, len(v)) for k, v in groupby(func, seq).items()])
+
+
 def frequencies(seq):
     """ Find number of occurrences of each value in seq
 
     >>> frequencies(['cat', 'cat', 'ox', 'pig', 'pig', 'cat'])  #doctest: +SKIP
     {'cat': 3, 'ox': 1, 'pig': 2}
+
+    See Also:
+        countby
+        groupby
     """
-    return dict([(k, len(v)) for k, v in groupby(identity, seq).items()])
+    return countby(identity, seq)
--- itertoolz/tests/test_recipes.py
+++ itertoolz/tests/test_recipes.py
@@ -1,4 +1,6 @@
-from itertoolz.recipes import frequencies
+from itertoolz.recipes import countby, frequencies
+
+def even(x): return x % 2 == 0
 
 def test_frequencies():
     assert (frequencies(["cat", "pig", "cat", "eel",
@@ -7,3 +9,7 @@ def test_frequencies():
     assert frequencies([]) == {}
     assert frequencies("onomatopoeia") == {"a": 2, "e": 1, "i": 1, "m": 1,
                                            "o": 4, "n": 1, "p": 1, "t": 1}
+
+def test_countby():
+    assert countby(even, [1, 2, 3]) ==  {True: 1, False: 2}
+    assert countby(len, ['cat', 'dog', 'mouse']) == {3: 2, 5: 1}
